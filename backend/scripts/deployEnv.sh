7ecec54de49fcdfc74d9fdb037d188ccbdfa7f0c
Artour Fire <shurnd32281@gmail.com>
2026-04-28 04:22:28 +0300

    fix: use ssh key for backend env copy

--- backend/scripts/deployEnv.sh
+++ backend/scripts/deployEnv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SSH_CONFIG="${1}"
 PROJECT_PATH="${2}"
-scp -Cr .env "$SSH_CONFIG:${PROJECT_PATH}/shared/.env"
+SSH_KEY_PATH="${3}"
+scp -i "$SSH_KEY_PATH" -Cr .env "$SSH_CONFIG:${PROJECT_PATH}/shared/.env"
