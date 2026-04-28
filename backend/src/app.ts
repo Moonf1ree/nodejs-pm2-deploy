b5f60dedca7a63584154870b486dd31abe84b32d
Artour Fire <shurnd32281@gmail.com>
2026-04-28 12:18:18 +0300

    feat: fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -13,8 +13,30 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-// Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+const allowedCors = [
+  'https://moonf1ree.nomorepartiessite.ru',
+  'http://moonf1ree.nomorepartiessite.ru',
+  'https://api.moonf1ree.nomorepartiessite.ru',
+  'http://api.moonf1ree.nomorepartiessite.ru',
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedCors.includes(origin)) {
+      callback(null, true);
+      return;
+    }
+
+    callback(new Error('CORS policy violation'));
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
