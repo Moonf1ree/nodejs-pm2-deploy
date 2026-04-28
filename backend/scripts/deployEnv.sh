25866d7003f380effbc0f2d6c78de29e4b5fb0f1
Artour Fire <shurnd32281@gmail.com>
2026-04-28 04:24:33 +0300

    feat: upd lock

--- backend/scripts/deployEnv.sh
+++ backend/scripts/deployEnv.sh
@@ -2,4 +2,18 @@
 SSH_CONFIG="${1}"
 PROJECT_PATH="${2}"
 SSH_KEY_PATH="${3}"
+
+if [ -z "$SSH_CONFIG" ] || [ -z "$PROJECT_PATH" ] || [ -z "$SSH_KEY_PATH" ]; then
+  if [ -f .env.deploy ]; then
+    DEPLOY_USER=$(awk -F= '/^DEPLOY_USER=/{print $2}' .env.deploy | tr -d '\r')
+    DEPLOY_HOST=$(awk -F= '/^DEPLOY_HOST=/{print $2}' .env.deploy | tr -d '\r')
+    DEPLOY_PATH=$(awk -F= '/^DEPLOY_PATH=/{print $2}' .env.deploy | tr -d '\r')
+    DEPLOY_KEY=$(awk -F= '/^SSH_KEY_PATH=/{print $2}' .env.deploy | tr -d '\r')
+
+    SSH_CONFIG="${SSH_CONFIG:-${DEPLOY_USER}@${DEPLOY_HOST}}"
+    PROJECT_PATH="${PROJECT_PATH:-$DEPLOY_PATH}"
+    SSH_KEY_PATH="${SSH_KEY_PATH:-$DEPLOY_KEY}"
+  fi
+fi
+
 scp -i "$SSH_KEY_PATH" -Cr .env "$SSH_CONFIG:${PROJECT_PATH}/shared/.env"
